9a5acb9b8972bc800a4ed3941afdf9e586e1ac55
mou-kexin <2584857586@qq.com>
2024-05-12 09:03:14 +0800

    作业代码提交

---
+++ Modify the definition of data/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            Data data = new Data(x, y);
+            System.out.println(data.getX() + data.getY());
+        }
+    }
+
+}
+
+class Data {
+
+    private int x;
+    private int y;
+
+    Data(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int getX() {
+        return x;
+    }
+
+    int getY() {
+        return y;
+    }
+
+}
