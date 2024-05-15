946b34ec482bc2f97c50900096608c3378408b64
mou-kexin <2584857586@qq.com>
2024-05-15 10:42:34 +0800

    作业测试代码

---
+++ 20240515homework/src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+
+        static int cnt = 6;
+        static{
+            cnt += 9;
+        }
+//        public static void main(String[] args){
+//
+//            System.out.println（"cnt = " + cnt);
+//        }
+        static{
+            cnt /=3;
+        };
+
+}
