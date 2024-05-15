c972015b943995fb743ff4b98ba277cfdf4b2489
mou-kexin <2584857586@qq.com>
2024-05-15 19:38:15 +0800

    代码提交

---
+++ Complement structural method/src/demo/Base.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class Base {
+    private int x;
+    private int y;
+
+    public Base(int x,int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
