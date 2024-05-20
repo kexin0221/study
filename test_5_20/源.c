92150316cba00374aa08dabda4345212e7c5647e
mou-kexin <2584857586@qq.com>
2024-05-20 22:15:51 +0800

    提交代码

---
+++ test_5_20/源.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("%s\n", __FILE__);
+	printf("%d\n", __LINE__);
+	printf("%s\n", __DATE__);
+	printf("%s\n", __TIME__);
+	//printf("&s\n", __STDC__);
+
+	return 0;
+}
