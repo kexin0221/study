950d2909b3bb3ddffa4c92019db9321a5f9af2c2
mou-kexin <2584857586@qq.com>
2024-06-05 11:37:39 +0800

    提交代码

---
+++ Decimal number to binary number/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        //write your code here......
+        System.out.println(Integer.toString(num,2));
+    }
+}
