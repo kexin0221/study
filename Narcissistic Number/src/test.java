ef14d3d2953b65e7c4317737e193fe6449b86b40
mou-kexin <2584857586@qq.com>
2024-10-06 17:08:03 +0800

    提交代码

--- Narcissistic Number/src/test.java
+++ Narcissistic Number/src/test.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
+
+//水仙花数
 public class test {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int i=0;
-        for(i=100;i<=n;i++) {
-            int ge=i%10;
-            int bai=i/100;
-            int shi=(i-bai*100)/10;
-            if(ge*ge*ge+shi*shi*shi+bai*bai*bai==i)
-            {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 0;
+        for (i = 100; i <= n; i++) {
+            int ge = i % 10;
+            int bai = i / 100;
+            int shi = (i - bai * 100) / 10;
+            if (ge * ge * ge + shi * shi * shi + bai * bai * bai == i) {
                 System.out.println(i);
             }
         }
